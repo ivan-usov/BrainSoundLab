638bffb31db135fd9486578631672d69ad16746b
Ivan Usov <ivan.a.usov@gmail.com>
2016-03-10 20:20:42 +0100

    Use the sweep times to determine spikes that account for a certain stimulus. This is implemented by redefining 'getSpikeTimings' and 'spike_count' functions, similar to FMS experiment type.

--- +experiment/FMS2xMix.m
+++ +experiment/FMS2xMix.m
@@ -32,7 +32,35 @@ classdef FMS2xMix < Block
             tMax = NaN;
         end
         
-        
+        function spikes = getSpikeTimings(this, t_min, t_max, group, chan)
+            if isnan(t_min) || isnan(t_max) % get spikes for the timeProcRange
+                % Find all spikes in a time window according to the sweepTime
+                
+                spikeTimings = this.spikeTimings{chan};
+                if ~isempty(this.filtSpikes)
+                    for k = 1:length(this.filtSpikes)
+                        filtParam = this.filtSpikes(k).val(this.filtSpikes(k).sel);
+                        map = ismember(this.filtSpikes(k).map{chan}, filtParam);
+                    end
+                    spikeTimings = spikeTimings(map);
+                end
+                
+                sweepTime = repmat(this.calcSweepTime(), 1, length(this.selRep));
+                
+                spikes = arrayfun(@spike_count, ...
+                    this.stimTimings(:, this.selRep, group), sweepTime, ...
+                    'UniformOutput', false);
+                
+            else % call the superclass method otherwise
+                spikes = getSpikeTimings@Block(this, t_min, t_max, group, chan);
+            end
+            
+            function spikes = spike_count(t0, sweepTime)
+                ind1 = find(spikeTimings >= t0, 1, 'first');
+                ind2 = find(spikeTimings <= t0 + sweepTime, 1, 'last');
+                spikes = spikeTimings(ind1:ind2) - t0;
+            end
+        end
     end
 end
 
