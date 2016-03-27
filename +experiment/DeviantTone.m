09ce11615802c9c0fbd7eed89fe4ea10243f87bd
Ivan Usov <ivan.a.usov@gmail.com>
2016-03-27 15:25:22 +0200

    Improve clearness of the functions

--- +experiment/DeviantTone.m
+++ +experiment/DeviantTone.m
@@ -8,9 +8,11 @@ classdef DeviantTone < Block
         function stimData = getStimuliConditions(this, source)
             stimData{1} = source.decB.data;
             this.stim(1).label = 'Level, dB';
-            
-            stimData{2} = source.freq.data/1000; % convert to kHz
-            this.stim(2).label = 'Frequency, kHz';
+        end
+        
+        function filtStimCond = getStimuliFilteringConditions(this, source)
+            filtStimCond{1} = source.freq.data/1000; % convert to kHz
+            this.filtStim(1).label = 'Frequency, kHz';
         end
     end
     
