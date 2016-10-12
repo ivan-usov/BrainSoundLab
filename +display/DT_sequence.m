5b25f806a256cb9a65a34c4c836fb424ba14cc6d
Ivan Usov <ivan.a.usov@gmail.com>
2016-10-12 16:21:09 +0200

    Add sequence analysis for Deviant Tone experiment

--- +display/DT_sequence.m
+++ +display/DT_sequence.m
@@ -5,3 +5,17 @@ block = BSL.block;
 ax = findobj('Tag', 'ax_dt_sequence');
 cla(ax);
 
+spikes_std_mean = block.custom.DT_standard_mean{BSL.curChannel};
+spikes_std_sigma = block.custom.DT_standard_sigma{BSL.curChannel};
+spikes_dev_mean = block.custom.DT_deviant_mean{BSL.curChannel};
+spikes_dev_sigma = block.custom.DT_deviant_sigma{BSL.curChannel};
+
+x = 0:length(spikes_std_mean)-1;
+
+hold(ax, 'on');
+errorbar(ax, x, spikes_std_mean, spikes_std_sigma, '.-', 'MarkerSize', 10);
+errorbar(ax, x, spikes_dev_mean, spikes_dev_sigma, '.-', 'MarkerSize', 10);
+
+legend(ax, 'Standard', 'Deviant');
+set(ax, 'XLim', [-1, length(spikes_std_mean)]);
+
--- +process/analyseDT.m
+++ +process/analyseDT.m
@@ -6,3 +6,55 @@ if nargin == 0
     chan = 1:block.nChannels;
 end
 
+% There should be only one group (see the experiment file)
+if group ~= 1
+    error('BrainSoundLab:analyseDT', 'There should be only 1 group');
+end
+
+% There should be only two tones
+if block.filtStim.len ~= 2
+    error('BrainSoundLab:analyseDT', 'There should be only 2 tones');
+end
+
+% Preallocate in case of the first time calculation
+if ~isfield(block.custom, 'DT_standard')
+    block.custom.DT_standard_mean = cell(1, block.nChannels);
+    block.custom.DT_standard_sigma = cell(1, block.nChannels);
+    block.custom.DT_deviant_mean = cell(1, block.nChannels);
+    block.custom.DT_deviant_sigma = cell(1, block.nChannels);
+end
+
+tones = block.filtStim.val;
+if nnz(block.filtStim.map == tones(1)) < nnz(block.filtStim.map == tones(2))
+    dev_tone = tones(1);
+else
+    dev_tone = tones(2);
+end
+
+is_dev = (block.filtStim.map == dev_tone);
+is_std = ~is_dev;
+
+is_dev_temp = is_dev;
+is_dev_temp(end) = 1; % account for non-devaint tone ending
+dev_pos = find(is_dev_temp); % positions of deviant tones
+std_seq_len = dev_pos - [0; dev_pos(1:end-1)] - 1; % sequence length of standard tones
+
+num_preced = []; % number of preceding standard tones
+for i = 1:length(std_seq_len)
+    num_preced = [num_preced; (0:std_seq_len(i)).']; %#ok<AGROW>
+end
+
+for l = chan
+    spikes_num = cellfun(@numel, block.spikesTimeProc{1}{l});
+    
+    for k = 1:max(std_seq_len)+1
+        spikes_std = spikes_num(num_preced == k-1 & is_std); % standard tones
+        block.custom.DT_standard_mean{l}(k) = mean(spikes_std);
+        block.custom.DT_standard_sigma{l}(k) = std(spikes_std);
+        
+        spikes_dev = spikes_num(num_preced == k-1 & is_dev); % deviant tones
+        block.custom.DT_deviant_mean{l}(k) = mean(spikes_dev);
+        block.custom.DT_deviant_sigma{l}(k) = std(spikes_dev);
+    end
+end
+
