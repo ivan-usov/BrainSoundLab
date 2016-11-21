8eccc6bed8016bf5fc868c62725168e3ed6a28cb
Tania Barkat <tania.barkat@unibas.ch>
2016-11-21 12:09:45 +0100

    "Created a new panel for comparing laser in tunung curves (sweeps), with parameters in the information panel
    information to be completed, legend to add"

--- +display/TuningCurves_Laser.m
+++ +display/TuningCurves_Laser.m
@@ -5,37 +5,58 @@ block = BSL.block;
 stim_lev = block.stim(1);
 stim_rate = block.stim(2);
 
-% ----- dsi
-ax = findobj('Tag', 'ax_dsi_n');
-result = block.custom.DSIwspont{BSL.curGroup}{BSL.curChannel};
-plot(stim_rate.val(stim_rate.len/2+1:end), result, 'Marker', '.', 'Parent', ax);
-legend(ax, num2str(stim_lev.val));
+% ----- Spike number as a fct of sweep rate at 40dB
+ax = findobj('Tag', 'ax_spikenb40_TuningCurvesLaser');
+resultoff = block.custom.TRFnum{1,1}{BSL.curChannel}(1,:);
+resulton = block.custom.TRFnum{1,2}{BSL.curChannel}(1,:);
+plot(stim_rate.val, resultoff, stim_rate.val, resulton,'Marker', '.', 'Parent', ax);
+legend(ax, 'off','on');
 
-% ----- dsi witout spont activity
-ax = findobj('Tag', 'ax_dsi_r');
-result = block.custom.DSIwospont{BSL.curGroup}{BSL.curChannel};
-plot(stim_rate.val(stim_rate.len/2+1:end), result, 'Marker', '.', 'Parent', ax);
+% ----- Spike rate as a fct of sweep rate at 40dB
+ax = findobj('Tag', 'ax_spikert40_TuningCurvesLaser');
+resultoff = block.custom.TRFrate{1,1}{BSL.curChannel}(1,:);
+resulton = block.custom.TRFrate{1,2}{BSL.curChannel}(1,:);
+plot(stim_rate.val, resultoff, stim_rate.val, resulton,'Marker', '.', 'Parent', ax);
+%legend(ax, 'off','on');
 
-% ----- lsi
-ax = findobj('Tag', 'ax_lsi_n');
-result = block.custom.LSIwspont{BSL.curGroup}{BSL.curChannel};
-plot(stim_rate.val, result, 'Marker', '.', 'Parent', ax);
+% ----- Spike number as a fct of sweep rate at 60dB
+ax = findobj('Tag', 'ax_spikenb60_TuningCurvesLaser');
+resultoff = block.custom.TRFnum{1,1}{BSL.curChannel}(2,:);
+resulton = block.custom.TRFnum{1,2}{BSL.curChannel}(2,:);
+plot(stim_rate.val, resultoff, stim_rate.val, resulton,'Marker', '.', 'Parent', ax);
+%legend(ax, 'off','on');
 
-% ----- lsi without spont activity
-ax = findobj('Tag', 'ax_lsi_r');
-result = block.custom.LSIwospont{BSL.curGroup}{BSL.curChannel};
-plot(stim_rate.val, result, 'Marker', '.', 'Parent', ax);
+% ----- Spike rate as a fct of sweep rate at 60dB
+ax = findobj('Tag', 'ax_spikert60_TuningCurvesLaser');
+resultoff = block.custom.TRFrate{1,1}{BSL.curChannel}(2,:);
+resulton = block.custom.TRFrate{1,2}{BSL.curChannel}(2,:);
+plot(stim_rate.val, resultoff, stim_rate.val, resulton,'Marker', '.', 'Parent', ax);
+%legend(ax, 'off','on');
 
-% ----- rsi_n
-ax = findobj('Tag', 'ax_rsi_n');
-result = block.custom.RSInum{BSL.curGroup}{BSL.curChannel};
-plot([-1, 1], result, 'Marker', '.', 'Parent', ax);
-set(ax, 'XTick', [-1, 1], 'XTickLabel', {'Down', 'Up'});
-legend(ax, num2str(stim_lev.val));
+% ----- Spike number as a fct of sweep rate at 80dB
+ax = findobj('Tag', 'ax_spikenb80_TuningCurvesLaser');
+resultoff = block.custom.TRFnum{1,1}{BSL.curChannel}(3,:);
+resulton = block.custom.TRFnum{1,2}{BSL.curChannel}(3,:);
+plot(stim_rate.val, resultoff, stim_rate.val, resulton,'Marker', '.', 'Parent', ax);
+%legend(ax, 'off','on');
 
-% ----- rsi_r
-ax = findobj('Tag', 'ax_rsi_r');
-result = block.custom.RSIrate{BSL.curGroup}{BSL.curChannel};
-plot([-1, 1], result, 'Marker', '.', 'Parent', ax);
-set(ax, 'XTick', [-1, 1], 'XTickLabel', {'Down', 'Up'});
+% ----- Spike rate as a fct of sweep rate at 80dB
+ax = findobj('Tag', 'ax_spikert80_TuningCurvesLaser');
+resultoff = block.custom.TRFrate{1,1}{BSL.curChannel}(3,:);
+resulton = block.custom.TRFrate{1,2}{BSL.curChannel}(3,:);
+plot(stim_rate.val, resultoff, stim_rate.val, resulton,'Marker', '.', 'Parent', ax);
+legend(ax, 'off','on');
+
+% ----- Update information
+cc = BSL.curChannel;
+inf_data = {
+    'DSI@75''', block.custom.DSIwspont{1,1}{1,cc}(13),block.custom.DSIwspont{1,2}{1,cc}(13),...
+    block.custom.DSIwspont{1,1}{1,cc}(14),block.custom.DSIwspont{1,2}{1,cc}(14),...
+    block.custom.DSIwspont{1,1}{1,cc}(15),block.custom.DSIwspont{1,2}{1,cc}(15), ''; ...
+    };
+
+set(findobj('Tag', 'tab_information'), 'Data', inf_data, ...
+    'ColumnName', {'Parameter', '40dB off','40dB on' '60dB off','60dB on','80dB off', '80dB on', ''}, ...
+    'ColumnFormat', {'char', 'bank', 'bank', 'bank','bank', 'bank', 'bank','char'});
+
 
